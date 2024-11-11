d5a1c797445b3959c2b6a02fee2d26182fbfe026
Edgar Ayala <ayalaalvarezedgarorlando@gmail.com>
2024-11-11 08:22:06 -0600

    avances en interfaces admin-factura y facturas

--- src/app/demo/ui-element/vistas/factura-administrador/factura-administrador.component.ts
+++ src/app/demo/ui-element/vistas/factura-administrador/factura-administrador.component.ts
@@ -3,14 +3,64 @@ import { MatCardModule } from '@angular/material/card';
 import { MatListModule } from '@angular/material/list';
 import { MatIconModule } from '@angular/material/icon';
 import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatSelectModule } from '@angular/material/select';
+import {FormsModule} from '@angular/forms';
+import { MatNativeDateModule} from '@angular/material/core';
 
+
+interface Food {
+  value: string;
+  viewValue: string;
+}
+
+interface Car {
+  value: string;
+  viewValue: string;
+}
+
+/**
+ * @title Basic select with initial value and no form
+ */
 @Component({
   selector: 'app-factura-administrador',
   standalone: true,
-  imports: [MatCardModule, MatListModule, MatIconModule, MatProgressSpinnerModule],
+  imports: [MatCardModule, MatListModule, MatIconModule, MatProgressSpinnerModule,MatFormFieldModule,
+    MatInputModule,MatDatepickerModule, MatSelectModule,  FormsModule,  MatNativeDateModule],
+
+
+
+
+
+
   templateUrl: './factura-administrador.component.html',
   styleUrl: './factura-administrador.component.scss'
 })
 export default class FacturaAdministradorComponent {
+  foods: Food[] = [
+    {value: 'steak-0', viewValue: 'San Vicente'},
+    {value: 'pizza-1', viewValue: 'San Salvador'},
+    {value: 'tacos-2', viewValue: 'San Miguel'},
+  ];
+  cars: Car[] = [
+    {value: 'ford', viewValue: '----'},
+    {value: 'chevrolet', viewValue: 'Contado'},
+    {value: 'dodge', viewValue: 'Credito'},
+  ];
+  selectedFood = this.foods[2].value;
+  selectedCar = this.cars[0].value;
+
+  selectCar(event: Event) {
+    this.selectedCar = (event.target as HTMLSelectElement).value;
+  }
 
 }
+
+
+
+
+
+
+
---
+++ src/app/demo/ui-element/vistas/factura/factura.component.ts
@@ -0,0 +1,71 @@
+
+import { MatCardModule } from '@angular/material/card';
+import { MatListModule } from '@angular/material/list';
+import { MatIconModule } from '@angular/material/icon';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatSelectModule } from '@angular/material/select';
+
+
+import {Component, OnInit} from '@angular/core';
+import {FormControl, FormsModule, ReactiveFormsModule} from '@angular/forms';
+import {Observable} from 'rxjs';
+import {map, startWith} from 'rxjs/operators';
+import {AsyncPipe} from '@angular/common';
+import {MatAutocompleteModule} from '@angular/material/autocomplete';
+
+
+export interface User {
+  name: string;
+}
+
+@Component({
+  selector: 'app-factura',
+  standalone: true,
+  imports: [MatCardModule, MatListModule, MatIconModule, MatProgressSpinnerModule,  MatFormFieldModule, MatInputModule, MatDatepickerModule, MatSelectModule, FormsModule, 
+    FormsModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatAutocompleteModule,
+    ReactiveFormsModule,
+    AsyncPipe,
+  ],
+
+  templateUrl: './factura.component.html',
+  styleUrl: './factura.component.scss'
+})
+export default class FacturaComponent implements OnInit{
+
+  value = 'Clear me';
+
+  
+
+  myControl = new FormControl<string | User>('');
+  options: User[] = [{name: 'Mary'}, {name: 'Shelley'}, {name: 'Igor'}];
+  filteredOptions!: Observable<User[]>;
+
+  ngOnInit() {
+    this.filteredOptions = this.myControl.valueChanges.pipe(
+      startWith(''),
+      map(value => {
+        const name = typeof value === 'string' ? value : value?.name;
+        return name ? this._filter(name as string) : this.options.slice();
+      }),
+    );
+  }
+
+  displayFn(user: User): string {
+    return user && user.name ? user.name : '';
+  }
+
+  private _filter(name: string): User[] {
+    const filterValue = name.toLowerCase();
+
+    return this.options.filter(option => option.name.toLowerCase().includes(filterValue));
+  }
+ 
+
+
+}
