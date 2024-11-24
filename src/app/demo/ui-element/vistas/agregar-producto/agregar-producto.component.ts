08942d643003486b50bdc6d736a31a36982c7803
Javier Ayala <franciscojavier20002017@gmail.com>
2024-11-24 11:13:20 -0600

    CRUD SUCURSAL

--- src/app/demo/ui-element/vistas/agregar-producto/agregar-producto.component.ts
+++ src/app/demo/ui-element/vistas/agregar-producto/agregar-producto.component.ts
@@ -9,7 +9,18 @@ import { BuscarUnidadMedidaComponent } from '../buscar-unidad-medida/buscar-unid
 })
 export default class AgregarProductoComponent {
   constructor(private modalService: NgbModal) {}
+  imagenPreview: string | null = null;
 
+  onImageSelected(event: Event): void {
+    const fileInput = event.target as HTMLInputElement;
+    if (fileInput.files && fileInput.files[0]) {
+      const reader = new FileReader();
+      reader.onload = () => {
+        this.imagenPreview = reader.result as string; // Vista previa de la imagen
+      };
+      reader.readAsDataURL(fileInput.files[0]); // Leer archivo como Data URL
+    }
+  }
   openModalAgregar (){
     const modalRef = this.modalService.open(BuscarUnidadMedidaComponent, {
       size: 'lg', // 'sm' | 'lg' | 'xl' para ajust
