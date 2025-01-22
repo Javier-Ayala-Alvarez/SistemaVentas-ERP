fd9322c0b32b2d526b2af5ffc5574c62d645c6e3
Javier Ayala <franciscojavier20002017@gmail.com>
2025-01-22 00:56:54 -0600

    cambios

--- src/app/demo/ui-element/services/clientes-services.service.ts
+++ src/app/demo/ui-element/services/clientes-services.service.ts
@@ -44,40 +44,7 @@ export class ClientesServicesService {
 
 
 
-<<<<<<< HEAD
-  // Eliminar el cliente
-         eliminar(id: number, cliente: ClienteClass): Observable<any> {
-          return new Observable(observer => {
-            Swal.fire({
-              title: 'Eliminar cliente',
-              text: '¿Estás seguro de que deseas eliminar este cliente?',
-              icon: 'warning',
-              showCancelButton: true,
-              confirmButtonColor: '#3085d6',
-              cancelButtonColor: '#d33',
-              confirmButtonText: 'Eliminar',
-              cancelButtonText: 'Cancelar'
-            }).then((resultado) => {
-              if (resultado.isConfirmed) {
-                cliente.estado = 'N'; // Marca el cliente como eliminado
-                this.httpClient.put(`${this.apiUrl}/Actualizar/${id}`, cliente).pipe(
-                  tap(() => {
-                    this.mensajeSwal2.mensaje('Eliminada exitoso', 'El cliente se ha modificado correctamente.');
-                    observer.next(true);  // Emite true indicando que la operación fue exitosa
-                  }),
-                  catchError((error) => {
-                    this.mensajeSwal2.handleError(error);
-                    observer.error(error);  // En caso de error, emite el error
-                    return throwError(error);
-                  })
-                ).subscribe();
-              } else {
-                observer.next(false);  // Si el usuario cancela, emite false
-              }
-            });
-          });
-        }
-=======
+
   // Eliminar Cliente
 eliminar(id: number, cliente: ClienteClass): Observable<any>  {
   return new Observable(observer => {
@@ -111,7 +78,6 @@ eliminar(id: number, cliente: ClienteClass): Observable<any>  {
   });
   });
 }
->>>>>>> b04da1dfbfd7ec2aaff3f25ddc0fb1fc1f6ba7f2
 
 // Muestra la lista de clientes
 load(search: string, page: number, size: number, order: string, asc: boolean): Observable<any> {
