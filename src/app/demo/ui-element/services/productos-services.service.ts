1816a269bac37706bc38198b8001b47a1edc9596
Javier Ayala <franciscojavier20002017@gmail.com>
2025-03-09 10:02:59 -0600

    Editarproducto

--- src/app/demo/ui-element/services/productos-services.service.ts
+++ src/app/demo/ui-element/services/productos-services.service.ts
@@ -1,11 +1,11 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { catchError, Observable, tap, throwError } from 'rxjs';
-import {loadConfig, baseUrl, imagenes} from '../services/helper';
+import { catchError, Observable, of, tap, throwError } from 'rxjs';
+import { loadConfig, baseUrl, imagenes } from '../services/helper';
 import { MensajesSwal2Service } from './mensajes-swal2.service';
 import { DatePipe } from '@angular/common';
 import Swal from 'sweetalert2';
-import { ProductoClass } from '../clases/producto-class'; 
+import { ProductoClass } from '../clases/producto-class';
 import { UnidadMedidaClass } from '../clases/unidad-medida-class';
 import { UnidadMedidaProductoClass } from '../clases/unidadMedidaProducto';
 import { Form } from '@angular/forms';
@@ -18,31 +18,25 @@ export class ProductosServicesService {
   private apiUrl = `${baseUrl}/Api/producto`; // Cambia la URL según sea necesario
 
   constructor(private httpClient: HttpClient, private mensajeSwal2: MensajesSwal2Service) { }
- 
+
   unidadMedidaProducto: UnidadMedidaProductoClass[] = [];
 
   agregarUnidadMedida(unidadMedidaProducto: UnidadMedidaProductoClass): void {
     this.unidadMedidaProducto.push({ ...unidadMedidaProducto });
   }
-  
+
   // Agrega un nuevo producto
   agregar(producto: ProductoClass, formData: FormData): Observable<any> {
     producto.estado = "A";
-    const form =  new FormData();
+    const form = new FormData();
     form.append('producto', JSON.stringify(producto));
-    form.append('imagen', formData.get('imagen') as Blob) ;
-    form.append('unidades',JSON.stringify(this.unidadMedidaProducto));
-      // Verificar si 'imagen' está en formData
-  if (formData.has('imagen')) {
     form.append('imagen', formData.get('imagen') as Blob);
-  } else {
-    console.error('No se ha añadido imagen');
-  }
+    form.append('unidades', JSON.stringify(this.unidadMedidaProducto));
+    
     producto.estado = 'A';
     return this.httpClient.post(`${this.apiUrl}/Guardar`, form).pipe(
       tap((respuesta: any) => {
         if (respuesta?.producto?.id) {
-          //this.AgregarUnidadMedida(respuesta.producto.id).subscribe(); // Llamada para agregar unidad de medida
           this.mensajeSwal2.mensaje('Guardado exitoso', 'El producto se ha guardado correctamente.');
         } else {
           this.mensajeSwal2.mensaje('Error', 'No se pudo obtener el ID del producto.');
@@ -51,70 +45,87 @@ export class ProductosServicesService {
       catchError(this.mensajeSwal2.handleError)
     );
   }
+
+  listaUnidadProducto(idProducto: number): Observable<any> {
+    return this.httpClient.get<any>(`${this.apiUrl}/ListUnidadProducto/${idProducto}`).pipe(
+      tap(dato => {
+        this.unidadMedidaProducto = dato; // Ahora se asigna correctamente
+      }),
+      catchError(error => {
+        this.mensajeSwal2.handleError(error);
+        return of([]); // Retorna un array vacío en caso de error
+      })
+    );
+  }
+  
+  
   
 
-  
-  
+
 
   // Modifica Producto
   modificar(id: number, producto: ProductoClass, formData: FormData): Observable<any> {
-    const form =  new FormData();
+    const form = new FormData();
     form.append('producto', JSON.stringify(producto));
-    form.append('imagen', formData.get('imagen') as Blob) ;
-    form.append('unidades',JSON.stringify(this.unidadMedidaProducto));
+    form.append('imagen', formData.get('imagen') as Blob);
+    form.append('unidades', JSON.stringify(this.unidadMedidaProducto));
     producto.estado = 'A';
     return this.httpClient.put(`${this.apiUrl}/Actualizar/${id}`, form).pipe(
       tap(() => {
-        this.mensajeSwal2.mensaje('Guardado exitoso','El producto se ha modificado correctamente.')
+        this.mensajeSwal2.mensaje('Guardado exitoso', 'El producto se ha modificado correctamente.')
       }),
-      catchError(this.mensajeSwal2.handleError) 
+      catchError(this.mensajeSwal2.handleError)
     );
   }
 
- // Eliminar el producto
-        eliminar(id: number, producto: ProductoClass): Observable<any> {
-          const form =  new FormData();
-          form.append('producto', JSON.stringify(producto));
-          form.append('unidades',JSON.stringify(this.unidadMedidaProducto));
-         return new Observable(observer => {
-           Swal.fire({
-             title: 'Eliminar Producto',
-             text: '¿Estás seguro de que deseas eliminar este producto?',
-             icon: 'warning',
-             showCancelButton: true,
-             confirmButtonColor: '#3085d6',
-             cancelButtonColor: '#d33',
-             confirmButtonText: 'Eliminar',
-             cancelButtonText: 'Cancelar'
-           }).then((resultado) => {
-             if (resultado.isConfirmed) {
-               producto.estado = 'N'; // Marca el productp como eliminado
-               this.httpClient.put(`${this.apiUrl}/Actualizar/${id}`, form).pipe(
-                 tap(() => {
-                   this.mensajeSwal2.mensaje('Eliminada exitoso', 'El producto se ha modificado correctamente.');
-                   observer.next(true);  // Emite true indicando que la operación fue exitosa
-                 }),
-                 catchError((error) => {
-                   this.mensajeSwal2.handleError(error);
-                   observer.error(error);  // En caso de error, emite el error
-                   return throwError(error);
-                 })
-               ).subscribe();
-             } else {
-               observer.next(false);  // Si el usuario cancela, emite false
-             }
-           });
-         });
-       }
+  // Eliminar el producto
+  eliminar(id: number, producto: ProductoClass): Observable<any> {
+    const form = new FormData();
+    form.append('producto', JSON.stringify(producto));
+    form.append('unidades', JSON.stringify(this.unidadMedidaProducto));
+    return new Observable(observer => {
+      Swal.fire({
+        title: 'Eliminar Producto',
+        text: '¿Estás seguro de que deseas eliminar este producto?',
+        icon: 'warning',
+        showCancelButton: true,
+        confirmButtonColor: '#3085d6',
+        cancelButtonColor: '#d33',
+        confirmButtonText: 'Eliminar',
+        cancelButtonText: 'Cancelar'
+      }).then((resultado) => {
+        if (resultado.isConfirmed) {
+          producto.estado = 'N'; // Marca el productp como eliminado
+          this.httpClient.put(`${this.apiUrl}/Actualizar/${id}`, form).pipe(
+            tap(() => {
+              this.mensajeSwal2.mensaje('Eliminada exitoso', 'El producto se ha modificado correctamente.');
+              observer.next(true);  // Emite true indicando que la operación fue exitosa
+            }),
+            catchError((error) => {
+              this.mensajeSwal2.handleError(error);
+              observer.error(error);  // En caso de error, emite el error
+              return throwError(error);
+            })
+          ).subscribe();
+        } else {
+          observer.next(false);  // Si el usuario cancela, emite false
+        }
+      });
+    });
+  }
 
-// Muestra la lista de gastos
-load(search: string, page: number, size: number, order: string, asc: boolean): Observable<any> {
-  return this.httpClient.get(`${this.apiUrl}/List?busqueda=${search}&page=${page}&size=${size}&order=${order}&asc=${asc}`).pipe(
-    catchError(this.mensajeSwal2.handleError) 
-  );
-}
+  // Muestra la lista de gastos
+  load(search: string, page: number, size: number, order: string, asc: boolean): Observable<any> {
+    return this.httpClient.get(`${this.apiUrl}/List?busqueda=${search}&page=${page}&size=${size}&order=${order}&asc=${asc}`).pipe(
+      catchError(this.mensajeSwal2.handleError)
+    );
+  }
 
-
+  productoLoad(id: number): Observable<any> {
+    return this.httpClient.get(`${this.apiUrl}/producto/${id}`).pipe(
+      catchError(this.mensajeSwal2.handleError)
+    );
+  }
 
 
 
--- src/app/demo/ui-element/vistas/agregar-producto/agregar-producto.component.ts
+++ src/app/demo/ui-element/vistas/agregar-producto/agregar-producto.component.ts
@@ -2,40 +2,46 @@ import { Component } from '@angular/core';
 import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
 import { NgbActiveModal } from '@ng-bootstrap/ng-bootstrap';
 import { BuscarUnidadMedidaComponent } from '../buscar-unidad-medida/buscar-unidad-medida.component';
-import { ProductosServicesService } from '../../services/productos-services.service';	
-import { ProductoClass } from '../../clases/producto-class'; 
-import { Router } from '@angular/router';
+import { ProductosServicesService } from '../../services/productos-services.service';
+import { ProductoClass } from '../../clases/producto-class';
+import { ActivatedRoute, Router } from '@angular/router';
 import { DatePipe } from '@angular/common';
 import { CategoriasServicesService } from '../../services/categorias-services.service';
 import { UnidadMedidaClass } from '../../clases/unidad-medida-class';
 import { UnidadMedidaProductoClass } from '../../clases/unidadMedidaProducto';
 import { UnidadesServicesService } from '../../services/unidades-services.service';
+import { loadConfig, baseUrl, imagenes } from '../../services/helper';
+
 @Component({
   selector: 'app-agregar-producto',
   templateUrl: './agregar-producto.component.html',
   styleUrl: './agregar-producto.component.scss'
 })
 export default class AgregarProductoComponent {
-    unidadMedidaProducto: UnidadMedidaProductoClass[] = [];
-  
-
+  unidadMedidaProducto: UnidadMedidaProductoClass[] = [];
+  imagenRuta: string = "";
   productoNuevo: ProductoClass = new ProductoClass(); // Inicialización por defecto
   producto?: ProductoClass; // Recibe la sucursal desde el componente principal
-  categorias:any[] =[];
-  categoriaSelect:any; 
+  categorias: any[] = [];
+  categoriaSelect: any;
   selectedImage: File | undefined; // Para almacenar el archivo de logo seleccionado
-
-  constructor(private modalService: NgbModal, private unidadesServices: UnidadesServicesService ,private productoService: ProductosServicesService,private categoria: CategoriasServicesService, private router: Router, private datePipe: DatePipe) {}
+  id: number = 0;
+  constructor(private modalService: NgbModal, private unidadesServices: UnidadesServicesService, private productoService: ProductosServicesService, private categoria: CategoriasServicesService, private router: Router, private datePipe: DatePipe, private route: ActivatedRoute, // Usamos ActivatedRoute aquí
+  ) { }
 
   //Valores de inicio
   ngOnInit(): void {
-    if (this.producto) {
-      this.productoNuevo = { ...this.producto }; // Copia los valores si está definido
-    } else {
-      this.productoNuevo = new ProductoClass(); // Asegura la inicialización
-    } 
-    this.loadCategoria();
+    this.imagenRuta = imagenes;
+    this.productoNuevo = new ProductoClass(); // Asegura la inicialización
+     this.id = this.route.snapshot.queryParams['id'];
     this.unidadMedidaProducto = this.productoService.unidadMedidaProducto;
+    console.log(this.id);
+    this.loadCategoria();
+    if(this.id > 0){
+      this.loadProducto(this.id);
+    }
+    
+
   }
   imagenPreview: string | null = null;
 
@@ -51,37 +57,30 @@ export default class AgregarProductoComponent {
       reader.readAsDataURL(fileInput.files[0]); // Leer archivo como Data URL
     }
   }
-  openModalAgregar (){
+  openModalAgregar() {
     const modalRef = this.modalService.open(BuscarUnidadMedidaComponent, {
       size: 'lg', // 'sm' | 'lg' | 'xl' para ajust
       centered: true
-  });
+    });
   }
 
   eliminarUnidadmedida(producto: UnidadMedidaProductoClass): void {
     if (this.producto?.id) {
-      // Simplemente asigna null a la propiedad unidadMedida
       producto.unidadMedida = undefined;
-      
-      // Si necesitas actualizar en backend
-       //this.productoService.modificar(producto.id, producto).subscribe(
-        // () => console.log('Unidad de medida eliminada'),
-        //error => console.error('Error al eliminar unidad de medida', error)
-       //);
     }
   }
 
 
   //Guardar Producto
-  guardar(){
+  guardar() {
     const formData = new FormData();
     // Verificar que selectedLogo no sea null y sea de tipo File
     if (this.selectedImage) {
       formData.append('imagen', this.selectedImage);
     }
-    
-    if (this.producto != null) {
-      this.productoService.modificar(this.productoNuevo.id ?? 0, this.productoNuevo, formData).subscribe(()=>
+
+    if (this.id >0) {
+      this.productoService.modificar(this.productoNuevo.id ?? 0, this.productoNuevo, formData).subscribe(() =>
         this.router.navigateByUrl('/', { skipLocationChange: true }).then(() => {
           this.router.navigate(['/component/productos']);
         }));
@@ -92,30 +91,46 @@ export default class AgregarProductoComponent {
           this.router.navigate(['/component/productos']);
         }));
     }
-    
+
 
   }
-  
-  //mostrar datos de la sucursal
-loadCategoria() {
-  this.categoria.listaCombo().subscribe(
-    (dato: any) => {
-      this.categorias = dato;
 
+  //mostrar datos de la sucursal
+  loadCategoria() {
+    this.categoria.listaCombo().subscribe(
+      (dato: any) => {
+        this.categorias = dato;
+        if (this.productoNuevo.categoria) {
+          this.productoNuevo.categoria = this.categorias?.find(emp => emp.id === this.productoNuevo.categoria?.id);
+        }
       }
 
-  );
-}
-onLogoSelect(event: Event): void {
-  const input = event.target as HTMLInputElement;
-  if (input?.files && input.files.length > 0) {
-    this.selectedImage = input.files[0]; // Guardar el archivo seleccionado
-    // Leer el archivo y generar una vista previa
-    const reader = new FileReader();
-    reader.onload = (e) => {
-      this.imagenPreview = e.target?.result as string; // Asignar la vista previa del archivo
-    };
-    reader.readAsDataURL(this.selectedImage);
+    );
   }
-}
+  loadProducto(id: number) {
+    this.productoService.productoLoad(id).subscribe(
+      (dato: any) => {
+        this.productoNuevo = dato;
+        this.loadCategoria();
+        this.productoService.listaUnidadProducto(id).subscribe(
+          (unidadMedida: any) => {
+            this.unidadMedidaProducto = unidadMedida;
+          });
+      }
+      
+    )
+
+  }
+  onLogoSelect(event: Event): void {
+    const input = event.target as HTMLInputElement;
+    if (input?.files && input.files.length > 0) {
+      this.selectedImage = input.files[0]; // Guardar el archivo seleccionado
+      // Leer el archivo y generar una vista previa
+      const reader = new FileReader();
+      reader.onload = (e) => {
+        this.imagenPreview = e.target?.result as string; // Asignar la vista previa del archivo
+      };
+      reader.readAsDataURL(this.selectedImage);
+    }
+  }
 }
