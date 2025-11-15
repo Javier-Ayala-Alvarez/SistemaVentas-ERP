c2200125a533b5497b1a02107053f93d9c104501
Javier Ayala <franciscojavier20002017@gmail.com>
2025-11-15 11:14:31 -0600

    variables de entorno

--- src/environments/environment.tomcat.ts
+++ src/environments/environment.tomcat.ts
@@ -1,4 +1,7 @@
+import packageInfo from '../../package.json';
+
 export const environment = {
+  appVersion: packageInfo.version,
   production: true,
   apiUrl: 'http://192.168.0.100:8080/venta',
   imagenesUrl: 'http://192.168.0.100:8080/imagenesAplicativo/'
