31d0882c39472d1f4f7dfcebcf0e9b330818ecd1
Javier Ayala <franciscojavier20002017@gmail.com>
2025-11-15 10:18:31 -0600

    variables de entorno

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,9 +1,8 @@
 import packageInfo from '../../package.json';
 
-
 export const environment = {
   appVersion: packageInfo.version,
   production: true,
   apiUrl: 'https://sistema-de-venta-erp-back-production.up.railway.app',
-  imagenesUrl: 'https://sistema-de-venta-erp-back-production.up.railway.app/imagenesAplicativo'
+  imagenesUrl: 'https://sistema-de-venta-erp-back-production.up.railway.app/app/imagenesAplicativo'
 };
