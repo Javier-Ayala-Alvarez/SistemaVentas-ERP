ccdfb0ea4816a5a72907729f0228bde0ffd89cf3
Javier Ayala <franciscojavier20002017@gmail.com>
2025-11-15 09:28:04 -0600

    variables de entorno

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -3,6 +3,6 @@ import packageInfo from '../../package.json';
 export const environment = {
   appVersion: packageInfo.version,
   production: true,
-  apiUrl: 'https://tu-backend.up.railway.app',
-  imagenesUrl: 'https://tu-backend.up.railway.app/imagenesAplicativo/'
+  apiUrl: 'https://sistema-de-venta-erp-back-production.up.railway.app/',
+  imagenesUrl: 'https://sistema-de-venta-erp-back-production.up.railway.app/imagenesAplicativo/'
 };
